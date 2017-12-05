4aea6d8183f0e6ac8f785534acf19bd1b97b9598
Rui Gamito <atrium601@gmail.com>
2017-12-05 23:34:49 +0000

    Add basic deploy-to-android script

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+APK_TOOL_DIR=./tools/StartGamedev-170112-osx
+BUILD_APK=make-apk.command
+FILES="block.lua main.lua parts.png player.lua world.lua"
+LOVE_FILE=game.love
+APK_FILE=game.apk
+
+echo "Zipping the files..."
+zip $LOVE_FILE $FILES
+echo "  - created $LOVE_FILE"
+mv $LOVE_FILE $APK_TOOL_DIR
+
+echo
+echo "Running packager..."
+cd $APK_TOOL_DIR
+open -g $BUILD_APK > /dev/null
+sleep 5
+
+if [ -f $APK_FILE ]; then
+  echo "  - APK created"
+else
+  echo "  - APK creation FAILED"
+  exit 1
+fi
+
+kill -9 $(cat PID)
+
+echo "Installing APK..."
+adb install $APK_FILE
