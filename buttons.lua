5f113540728c722a0407b47c5dc3ad3c3e8e5366
Rui Gamito <atrium601@gmail.com>
2017-12-21 10:48:28 +0000

    Adding buttons...
    - not scaled yet (meaning they will be really small on android)

---
+++ buttons.lua
@@ -0,0 +1,36 @@
+
+
+function addButtons()
+
+  ---------------------------------- PLAY AGAIN
+  local playAgainBtn = UIButton:new()
+  playAgainBtn:setPos(200, 300)
+  playAgainBtn:setWidth(200)
+  playAgainBtn:setText("Press to play again")
+  playAgainBtn:setAnchor(0, 0)
+  --playAgainBtn:setEnabled(true)
+  playAgainBtn:setVisible(false)
+  playAgainBtn.press = function()
+    -- hide the button when pressed
+    playAgainBtn:setVisible(false)
+    world.play()
+  end
+  world.buttons["play_again"] = playAgainBtn
+
+
+  ---------------------------------- START GAME
+  local startGame = UIButton:new()
+  startGame:setPos(200, 300)
+  startGame:setWidth(200)
+  startGame:setText("Start game")
+  startGame:setAnchor(0, 0)
+  startGame:setEnabled(true)
+  startGame:setVisible(true)
+  startGame.press = function()
+    -- hide the button when pressed
+    startGame:setVisible(false)
+    world.play()
+  end
+  world.buttons["start_game"] = startGame
+
+end
