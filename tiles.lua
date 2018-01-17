436f56526850da83f97038aeb65302cd80714197
Carolina Costa <carolina.costa@MPT-00897.local>
2018-01-17 10:39:39 +0000

    no message

---
+++ tiles.lua
@@ -0,0 +1,328 @@
+TILE_001 = {
+  meta = {
+    rotate = false,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 15, y = 28, w = 17, h = 17},
+  }
+}
+
+TILE_002 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 45, y = 5 , w = 5, h = 5},
+  }
+}
+
+TILE_003 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+  { x = 76, y = 5 , w = 5, h = 20},
+  }
+}
+
+TILE_004 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+     { x = 10, y = 00, w = 15, h = 15},
+  }
+}
+
+TILE_005 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+     { x = 10, y = 20, w = 10, h = 19},
+  }
+}
+
+TILE_006 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+    { x = 23, y = 45, w = 10, h = 20},
+  }
+}
+
+TILE_007 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+      { x = 10, y = 67, w = 10, h = 12},
+      { x = 35, y = 44, w = 5, h = 15},
+
+  }
+}
+
+TILE_008 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 30, y = 10, w = 9, h = 9},
+   { x = 68, y = 17, w = 4, h = 4},
+   { x = 5, y = 85, w = 4, h = 8},
+   { x = 15, y = 44, w = 7, h = 9}
+
+  }
+}
+
+TILE_009 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 20, y = 5, w = 15, h = 15},
+
+  }
+}
+
+TILE_010 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 10, y = 15, w = 5, h = 20},
+
+  }
+}
+
+
+TILE_011 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 15, y = 25, w = 5, h = 25},
+
+  }
+}
+
+TILE_012 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 13, y = 9, w = 8, h = 20},
+
+  }
+}
+
+TILE_013 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 5, y = 10, w = 5, h = 5},
+   { x = 15, y = 15, w = 5, h = 5},
+  }
+}
+
+TILE_014 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 5, y = 7, w = 5, h = 5},
+   { x = 10, y = 23, w = 5, h = 5},
+   { x = 15, y = 5, w = 5, h = 5},
+  }
+}
+
+TILE_015 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 23, y = 10, w = 14, h = 14},
+   { x = 55, y = 30, w = 7, h = 7},
+  }
+}
+
+TILE_016 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 12, y = 12, w = 12, h = 12},
+
+  }
+}
+
+TILE_017 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 8, y = 7, w = 16, h = 25},
+  }
+}
+
+TILE_018 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 47, y = 13, w = 8, h = 8},
+  }
+}
+
+TILE_019 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+  { x = 20, y = 22, w = 5, h = 5},
+  }
+}
+
+TILE_020 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+   { x = 50, y = 13, w = 4, h = 25},
+  }
+}
+
+TILE_021 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 100
+  },
+  rect = {
+  { x = 15, y = 32, w = 12, h = 18},
+  }
+}
+
+TILE_022 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+  { x = 75, y = 13, w = 15, h = 4},
+  }
+}
+
+TILE_023 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+  { x = 35, y = 25, w = 2, h = 10},
+  }
+}
+
+TILE_024 = {
+  meta = {
+    rotate = true,
+    top = true,
+    bottom = true,
+    weight = 1
+  },
+  rect = {
+  { x = 45, y = 74, w = 4, h = 15},
+  }
+}
+TILES = {
+  TILE_001,
+  TILE_002,
+  TILE_003,
+  TILE_004,
+  TILE_005,
+  TILE_006,
+  TILE_007,
+  TILE_008,
+  TILE_009,
+  TILE_010,
+  TILE_011,
+  TILE_012,
+  TILE_013,
+  TILE_014,
+  TILE_015,
+  TILE_016,
+  TILE_017,
+  TILE_018,
+  TILE_019,
+  TILE_021,
+  TILE_022,
+  TILE_023,
+  TILE_024
+}
